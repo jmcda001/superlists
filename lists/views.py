b483b4c8b75dfa78fa146af25eb11bf418d8df97
Jeffrey Work <jmcda001@github.com>
2017-09-08 16:04:09 -0700

    Added first database

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+
+from lists.models import Item
 
 # Create your views here.
 def home_page(request):
-    return HttpResponse('<html>')
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
+
+    context = {'items': Item.objects.all()}
+    return render(request,'lists/home.html',context)
